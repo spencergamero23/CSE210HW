6d032a3ee2938880f13b453054193bef77382e9f
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2023-04-20 17:02:08 -0600

    first assignment finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        console.write("What is your first name?");
+        string first_name = console.readline();
+        
+        console.write("What is your last name?");
+        string last_name = console.readline();
+
+        console.writeline($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
