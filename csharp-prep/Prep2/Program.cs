3ae26e6a3646b7ba9af3640812ab912fb0695b91
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2023-04-20 18:58:53 -0600

    done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        console.write("What is your grade percentage?");
+        string userGrade = console.readline();
+
+        int grade = int.parse(userGrade);
+        string letter = ""
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B"
+        }
+        else if (grade >=70)
+        {
+            letter = "C"
+        }
+        else if (grade >= 60)
+        {
+            letter = "D"
+        }
+        else
+        {
+            letter = "F"
+        }
+
+        console.writeline($"You got a {letter}")
+
+        if (grade >= 70)
+        {
+            console.writeline("You passed the class!")
+        }
+        else
+        {
+            console.writeline("You failed")
+        }
     }
 }
