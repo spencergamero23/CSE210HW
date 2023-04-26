910c0a9d71290e6f39aa75d061d84c245bd13805
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2023-04-26 10:01:46 -0600

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //First find the random number//
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        int userInput = 0;
+        //1st ask user for random number//
+       do{
+
+        Console.Write("Enter your guess for the magic number ");
+        userInput = Convert.ToInt32(Console.ReadLine());
+
+        //Response if userInput is higher than magicNumber//
+        if(userInput < magicNumber)
+        {
+            Console.WriteLine("Higher!");
+        }
+        //Response if userInput is higher than magicNumber//
+        else if (userInput > magicNumber)
+        {
+            Console.WriteLine("Lower!");
+        }
+        
+        //Creat while loop to continue asking for the magic number until it finds it!//
+        } while (userInput != magicNumber);
+
+        Console.WriteLine("You got it!");
     }
 }
