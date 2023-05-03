a6f8884b7629773f036e5d1fce6fce6994e9c1b3
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2023-05-02 20:28:06 -0600

    Finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2006;
+        job1._endYear = 2016;
+
+        Job job2 = new Job();
+        job2._company = "Boeing";
+        job2._jobTitle = "Construction";
+        job2._startYear = 2004;
+        job2._endYear = 2023;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Spencer Gamero";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
+
+        
     }
 }
